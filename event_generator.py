0f718aa6dc67a397e7cb38bf1dc293cf8c0c9ba3
Lazy-ZMS <87305241+Lazy-ZMS@users.noreply.github.com>
2021-07-12 14:50:32 +0500

    Update and rename Event_Generator.py to event_generator.py

--- event_generator.py
+++ event_generator.py
@@ -1,5 +1,5 @@
 # sample.py
-from concurrency import coroutine, sleep, run
+from generator_core import coroutine, sleep, run
 import shutil # Подключаем модуль
 import glob
 import os.path
@@ -19,8 +19,8 @@ def copy_File_from_kass(timeout):
             shutil.copy(filename, dest_dir)
             
 
-##hello("Петров", 1)
+hello("Петров", 1)
 ##hello("Иванов", 3.0)
 ##hello("Мир", 5.0)
-copy_File_from_kass(0.5)
+##copy_File_from_kass(0.5)
 run()
